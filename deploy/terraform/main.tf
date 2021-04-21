0df952846e7473a94df7aebb083dd1e2bc7c515b
pmRz00 <tpstiller@gmx.de>
2021-04-21 08:42:46 +0000

    sp Terraform authentication

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,5 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.56.0"
+    }
+  }
+}
+
 provider "azurerm" {
-    features {}
+  features {}
+  subscription_id = ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+  client_id       = ${{ secrets.AZURE_AD_CLIENT_ID }}
+  client_secret   = ${{ secrets.AZURE_AD_CLIENT_SECRET }}
+  tenant_id       = ${{ secrets.AZURE_AD_TENANT_ID }}
 }
 
 resource "azurerm_resource_group" "demo" {
