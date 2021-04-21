4b017c1a56b86f06533b3d8bfc698303b4faab1b
pmRz00 <tpstiller@gmx.de>
2021-04-21 08:48:38 +0000

    switched to env vars for tf auth with azure

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -9,10 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = ${{ secrets.AZURE_SUBSCRIPTION_ID }}
-  client_id       = ${{ secrets.AZURE_AD_CLIENT_ID }}
-  client_secret   = ${{ secrets.AZURE_AD_CLIENT_SECRET }}
-  tenant_id       = ${{ secrets.AZURE_AD_TENANT_ID }}
 }
 
 resource "azurerm_resource_group" "demo" {
